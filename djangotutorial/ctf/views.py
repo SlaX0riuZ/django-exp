9915f3d8d270616527b712ee6483ba66f178455b
SlaX0riuZ <slaxoriuz@gmail.com>
2025-04-22 11:28:13 -0500

    4/22/25 push to partial fix datetime issues

--- djangotutorial/ctf/views.py
+++ djangotutorial/ctf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import *
 from .forms import *
-from time import *
+from django.utils import timezone
 
 # Create your views here.
 def home(request): return render(request, 'index.html')
